99970a973f7c68b7fb9b02ecb3d949a532d4c793
YongWoo An <dyddn2015@gmail.com>
2025-01-03 20:12:13 +0900

    01.03

--- app/src/main/java/com/example/kotlincodingtest/01.03.kt
+++ app/src/main/java/com/example/kotlincodingtest/01.03.kt
@@ -1,76 +1,41 @@
 package com.example.kotlincodingtest
 
 /**
-백준 5639 이진 검색 트리 (골드4)
+백준 12851 숨바꼭질 2 (골드4)
 
-유형: 자료구조
-
-1. 이진탐색트리 일부 구현
-2. 전위순회 결과 트리에 주입
-3. 후위순회 진행
+1. BFS를 통한 완전탐색
+2. 동생의 위치가 더 작거나 같으면 걸어가는 경우가 항상 가장 빠르므로 N-K
+3. 동생의 위치가 더 클경우, 걷는 경우와 순간이동하는 경우를 queue에 추가
+4. check 리스트로 각 node에서의 최소시간을 update해 queue에 중복 없게함
+5. 최단시간이 나오면 그 후로는 종료
 */
 
-data class Node(val key: Int, var left: Node? = null, var right: Node? = null)
-
-class BinarySearchTree(var root: Node? = null) {
-
-    fun add(key: Int) {
-        if (this.root == null) {
-            this.root = Node(key=key)
-            return
-        } else {
-            val leafNode = getLeafNode(node=this.root!!, key=key)
-            if (key<leafNode.key) {
-                leafNode.left = Node(key=key)
-            } else {
-                leafNode.right = Node(key=key)
-            }
-        }
-    }
-
-    private fun getLeafNode(node: Node, key: Int): Node {
-        return if (key<node.key) {
-            if (node.left==null) {
-                node
-            } else {
-                getLeafNode(node=node.left!!, key=key)
-            }
-        } else {
-            if (node.right==null) {
-                node
-            } else {
-                getLeafNode(node=node.right!!, key=key)
-            }
-        }
-    }
-
-    fun postorderTraversal() {
-        fun printKey(node: Node) {
-            if (node.left!=null) {
-                printKey(node=node.left!!)
-            }
-            if (node.right!=null) {
-                printKey(node=node.right!!)
-            }
-            println(node.key)
-        }
-        if (this.root!=null) {
-            printKey(node=this.root!!)
-        }
-    }
-}
-
 fun main() = with(System.`in`.bufferedReader())  {
-    val tree = BinarySearchTree()
-    val lst = ArrayList<Int>()
-
-    while(true) {
-        try {
-            tree.add(key=readLine().toInt())
-        } catch(e: Exception) {
-            break
+    val (n, k) = readLine().split(" ").map { it.toInt() }
+    if (n>=k) {
+        println(n-k)
+        println(1)
+        return
+    }
+    val q = ArrayDeque<IntArray>()
+    q.addLast(intArrayOf(0, n))
+    val check = MutableList(k+2) { 1000000 }
+    check[n] = 0
+    var count = 0
+    while(q.isNotEmpty()) {
+        var (totalSec, node) = q.removeFirst()
+        if (node==k) {
+            count += 1
+            continue
+        }
+        totalSec += 1
+        for (nextNode in intArrayOf(node+1, node-1, node*2)) {
+            if (0<=nextNode && nextNode<k+2 && totalSec<=check[nextNode]) {
+                check[nextNode] = totalSec
+                q.addLast(intArrayOf(totalSec, nextNode))
+            }
         }
     }
-
-    tree.postorderTraversal()
+    println(check[k])
+    println(count)
 }
