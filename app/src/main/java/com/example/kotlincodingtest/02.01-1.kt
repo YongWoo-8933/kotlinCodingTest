fb31f3a7e6e0e9409bff76ecc073cff3b3d1f889
YongWoo An <dyddn2015@gmail.com>
2025-02-01 23:05:29 +0900

    02.01

---
+++ app/src/main/java/com/example/kotlincodingtest/02.01-1.kt
@@ -0,0 +1,34 @@
+package com.example.kotlincodingtest
+
+/**
+백준 1806 부분합 (골드4)
+
+1. 수열합의 범위를 지정할 좌우 포인터 lp, rp와 수열합 sequence_sum을 0, 0, 0으로 설정
+2. sequence_sum이 S보다 작으면 sequence[rp]값은 수열합에 더하고 rp를 오른쪽으로 이동
+3. sequence_sum이 S보다 커지면 answer를 갱신하고 수열합에서 sequence[lp]값을 뺀 뒤 lp를 오른쪽으로 이동
+4. lp가 rp보다 커지거나 rp가 범위를 벗어나면 종료
+ */
+
+import kotlin.math.*
+
+fun main() {
+    val (n, s) = readln().split(" ").map{ it.toInt() }
+    val lst = readln().split(" ").map{ it.toInt() }.toIntArray()
+    var answer = Int.MAX_VALUE
+    var lp = 0; var rp = 0; var sum = 0
+    while(lp<=rp) {
+        when {
+            sum<s && rp<n -> {
+                sum += lst[rp]
+                rp += 1
+            }
+            s<=sum -> {
+                answer = min(answer, rp-lp)
+                sum -= lst[lp]
+                lp += 1
+            }
+            else -> break
+        }
+    }
+    print(if(answer==Int.MAX_VALUE) 0 else answer)
+}
---
+++ app/src/main/java/com/example/kotlincodingtest/02.01-2.kt
@@ -0,0 +1,41 @@
+package com.example.kotlincodingtest
+
+/**
+백준 20040 사이클 게임 (골드4)
+
+1. 사이클 발생 여부를 따져야하므로, union-find 알고리즘 활용
+2. 전형적인 union-find 알고리즘 구현 후, 사이클이 발견되면 종료하고 몇번째인지 출력
+ */
+
+import kotlin.math.*
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val (n, m) = readLine().split(" ").map{ it.toInt() }
+    val roots = Array(n) { it }
+
+    fun find(x: Int): Int {
+        if(roots[x]!=x) {
+            roots[x] = find(roots[x])
+        }
+        return roots[x]
+    }
+
+    fun union(a: Int, b: Int) {
+        val ra = find(a); val rb = find(b)
+        if(ra!=rb) {
+            roots[max(ra, rb)] = min(ra, rb)
+        }
+    }
+
+    var answer = 0
+    for(turn in 1..m) {
+        val (a, b) = readLine().split(" ").map{ it.toInt() }
+        if (find(a)!=find(b)) {
+            union(a, b)
+        } else {
+            answer = turn
+            break
+        }
+    }
+    print(answer)
+}
