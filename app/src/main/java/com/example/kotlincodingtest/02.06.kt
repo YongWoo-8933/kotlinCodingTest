283560e408efb52069264b00393a2c77f8a4de28
YongWoo An <dyddn2015@gmail.com>
2025-02-06 21:04:48 +0900

    02.08

---
+++ app/src/main/java/com/example/kotlincodingtest/02.06.kt
@@ -0,0 +1,16 @@
+package com.example.kotlincodingtest.file0206
+
+/**
+백준 15787 기차가 어둠을 헤치고 은하수를 (실버2)
+
+1. 비트마스킹으로 기차 상태를 기록
+2. 명령이 끝나면 set으로 만들어 중복을 없애고 기차수를 return
+*/
+
+fun main() = with(System.`in`.bufferedReader()) {
+    val n = readln().toInt()
+    val nextList = mutableListOf(intArrayOf(0, 0))
+    nextList.
+    val lst = mutableListOf(3)
+    lst.size
+}
--- app/src/main/java/com/example/kotlincodingtest/코테대비용.kt
+++ app/src/main/java/com/example/kotlincodingtest/코테대비용.kt
@@ -1,8 +1,15 @@
 package com.example.kotlincodingtest.test
 
 import java.util.*
+import kotlin.math.*
+import kotlin.system.exitProcess
 
 fun main() {
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Int, Long 범위 제발 확인하기!!!!!!!!!!!!!!!!!!!!!!!!!
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
     // 정렬 메소드
     val lst = mutableListOf(1, 2, 4, 3)
     lst.sorted()
@@ -25,6 +32,11 @@ fun main() {
     map.putIfAbsent(5, "5") // 5에 값이 없으므로 셋
     map.getOrDefault(6, 0) // 6에 값이 없으므로 0반환
     map.getOrDefault(1, 0) // 1에 값이 있으므로 1반환
+
+    // Set
+    val set = mutableSetOf(3, 3, 2, 4)
+    set.addAll(mutableSetOf(1, 1, 3, 5))
+    set.removeAll(mutableSetOf(1, 1, 3, 5))
 
     // 배열 메소드
     lst.sum()
@@ -55,4 +67,27 @@ fun main() {
     val isSame = bits == (bits or (1 shl 2)) // 세번째 비트 켜져 있는지 확인
     bits = bits xor (1 shl 2) // 세번째 비트 토글
 
+    // math 메소드
+    2.0.pow(30)
+    sqrt(36.0)
+    log(8.0, 2.0)
+    log2(9.0)
+    abs(-3)
+
+    // str 관련 메소드
+    val sb = StringBuilder()
+    sb.append("x")
+    sb.appendLine("xxs")
+    sb.startsWith("dsds")
+    sb.endsWith("dsds")
+    sb.delete(1, 3)  // 1 until 3
+    sb.indexOf("s")
+    sb.replace(0, 3, "fs")
+    "dWgwgWAF".uppercase()
+    "dWgwgWAF".lowercase()
+    'F'.isUpperCase()
+    'F'.isLowerCase()
+    "Fdsafdsa".indexOf('a')
+    // 프로그램 종료
+    exitProcess(0)
 }
