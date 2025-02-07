3ab25ab47b15c991530cb0b03e5c6b2826ceb0b4
YongWoo An <dyddn2015@gmail.com>
2025-02-07 13:22:54 +0900

    02.07

--- app/src/main/java/com/example/kotlincodingtest/02.06.kt
+++ app/src/main/java/com/example/kotlincodingtest/02.06.kt
@@ -5,12 +5,32 @@ package com.example.kotlincodingtest.file0206
 
 1. 비트마스킹으로 기차 상태를 기록
 2. 명령이 끝나면 set으로 만들어 중복을 없애고 기차수를 return
+1 1 1
+1
+0
 */
 
 fun main() = with(System.`in`.bufferedReader()) {
-    val n = readln().toInt()
-    val nextList = mutableListOf(intArrayOf(0, 0))
-    nextList.
-    val lst = mutableListOf(3)
-    lst.size
+    val (n, m, _) = readLine().split(" ").map{ it.toInt() }
+    val exArr = Array(n+1){ if(it>0) readLine().toInt() else 0 }
+    val newArr = Array(m+1){ if(it>0) readLine().toInt() else 0 }
+    var answerCnt = 0; var answerCost = 0; var newFloor = 1
+    for(exFloor in 1..n) {
+        var itemCnt = exArr[exFloor]
+        while(itemCnt>0 && newFloor<=m) {
+            if(itemCnt<newArr[newFloor]) {
+                answerCnt += itemCnt
+                answerCost += (exFloor+newFloor)*itemCnt
+                newArr[newFloor] -= itemCnt
+                itemCnt = 0
+            } else {
+                answerCnt += newArr[newFloor]
+                answerCost += (exFloor+newFloor)*newArr[newFloor]
+                itemCnt -= newArr[newFloor]
+                newFloor += 1
+            }
+        }
+        if(itemCnt>0 || newFloor>m) break
+    }
+    print("$answerCnt $answerCost")
 }
