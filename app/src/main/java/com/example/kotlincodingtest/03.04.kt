80b2b31473e610aa72855d47a843bc98874fdf9e
YongWoo An <dyddn2015@gmail.com>
2025-03-04 22:56:03 +0900

    03.04

--- app/src/main/java/com/example/kotlincodingtest/03.04.kt
+++ app/src/main/java/com/example/kotlincodingtest/03.04.kt
@@ -3,7 +3,10 @@ package com.example.kotlincodingtest.file0304
 /**
  * 백준 15919 사자는 여행왕이야!! (골드 3)
  *
- * 1. 단순 수학 계산
+ * 1. dp+dfs조합으로 풀이
+ * 2. 먼저 여행 시작일자 순으로 일정 정렬
+ * 3. dfs로 모든 일정을 순회함.
+ * 4. 이때 dp로 특정 여행을 갈 경우 이후 나올 수 있는 최소 간격을 저장해둠
 */
 import kotlin.math.*
 
@@ -15,6 +18,12 @@ fun main() = with(System.`in`.bufferedReader()) {
     val dp = Array(m){ Int.MAX_VALUE }
 
     fun dfs(idx: Int, today: Int): Int {
-
+        if(idx==m) return n-today
+        val (fr, to) = sortedArr[idx]
+        if(fr<=today) return dfs(idx+1, today)
+        if(dp[idx]==Int.MAX_VALUE) dp[idx] = dfs(idx+1, to)
+        return min(dfs(idx+1, today), max(fr-today-1, dp[idx]))
     }
+
+    print(dfs(0, 0))
 }
