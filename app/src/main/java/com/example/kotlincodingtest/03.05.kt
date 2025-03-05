83cd49bb16f5a812cb1f765f95f70aa8d19f84f0
YongWoo An <dyddn2015@gmail.com>
2025-03-06 00:01:55 +0900

    03.05

--- app/src/main/java/com/example/kotlincodingtest/03.05.kt
+++ app/src/main/java/com/example/kotlincodingtest/03.05.kt
@@ -1,29 +1,40 @@
-package com.example.kotlincodingtest.file0304
+package com.example.kotlincodingtest.file0305
 
 /**
- * 백준 15919 사자는 여행왕이야!! (골드 3)
+ * 프로그래머스 합승택시요금 (level3)
  *
- * 1. dp+dfs조합으로 풀이
- * 2. 먼저 여행 시작일자 순으로 일정 정렬
- * 3. dfs로 모든 일정을 순회함.
- * 4. 이때 dp로 특정 여행을 갈 경우 이후 나올 수 있는 최소 간격을 저장해둠
+ * 1. 플로이드-워셜 알고리즘으로 각 노드간 최단비용을 미리 구함
+ * 2. 두 사람이 헤어질 곳(경유지)을 정하고, 그곳을 기준으로 택시비 산정
+ * 3. 경유지를 1~n까지 순회하며 최소 택시비용 찾기
 */
-import kotlin.math.*
+import kotlin.math.min
 
-fun main() = with(System.`in`.bufferedReader()) {
-    val n = readLine().toInt()
-    val m = readLine().toInt()
-    val arr = Array(m) { readLine().split(" ").map{ it.toInt() }.toTypedArray() }
-    val sortedArr = arr.sortedWith(compareBy({it.first()}, {it.last()})).toTypedArray()
-    val dp = Array(m){ Int.MAX_VALUE }
-
-    fun dfs(idx: Int, today: Int): Int {
-        if(idx==m) return n-today
-        val (fr, to) = sortedArr[idx]
-        if(fr<=today) return dfs(idx+1, today)
-        if(dp[idx]==Int.MAX_VALUE) dp[idx] = dfs(idx+1, to)
-        return min(dfs(idx+1, today), max(fr-today-1, dp[idx]))
+class Solution {
+    fun solution(n: Int, s: Int, a: Int, b: Int, fares: Array<IntArray>): Int {
+        val INF = 199*100000+1
+        val distance = Array(n+1) { Array(n+1) { INF } }
+        // 대각선 초기화
+        for (i in 1..n) {
+            distance[i][i] = 0
+        }
+        // 간선 정보 초기화
+        for ((c, d, f) in fares) {
+            distance[c][d] = f
+            distance[d][c] = f
+        }
+        // 플로이드 워셜 알고리즘
+        for (k in 1..n) {
+            for (i in 1..n) {
+                for (j in 1..n) {
+                    distance[i][j] = min(distance[i][j], distance[i][k]+distance[k][j])
+                }
+            }
+        }
+        // 경유지 들러 도착하는 택시요금 계산
+        var answer = INF
+        for (k in 1..n) {
+            answer = min(answer, distance[s][k]+distance[k][a]+distance[k][b])
+        }
+        return answer
     }
-
-    print(dfs(0, 0))
 }
